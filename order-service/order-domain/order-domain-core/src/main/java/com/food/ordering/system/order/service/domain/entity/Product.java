73bbcf8e4486923550b3576ce9821f2acc8b4ff7
zahra <zara.taghizadeh@gmail.com>
2023-07-22 14:35:17 +0330

    add Order Event
     Implementing Order Domain Service
    change product
    implementation OrderDomainService
    
    add dependency lombok and logging

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -9,7 +9,7 @@ public class Product extends BaseEntity<ProductId> {
     private String name;
     private Money price;
 
-    public Product(ProductId productId,String name, Money price) {
+    public Product(ProductId productId, String name, Money price) {
         super.setId(productId);
         this.name = name;
         this.price = price;
@@ -21,5 +21,10 @@ public class Product extends BaseEntity<ProductId> {
 
     public Money getPrice() {
         return price;
+    }
+
+    public void updateWithConfirmedNameAndPrice(String name, Money price) {
+        this.name = name;
+        this.price = price;
     }
 }
